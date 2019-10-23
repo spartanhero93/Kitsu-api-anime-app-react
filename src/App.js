9f406b02bb5fbb973fcc4786c675d6fbf07f5d5b
Luis Lopez <name.luislopez@gmail.com>
2019-10-22 22:07:50 -0400

    switching to personal server api calls

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ import './App.css'
 function App () {
   const [data, setData] = useState({})
 
-  useEffect(() => {
-    axios.get(`https://kitsu.io/api/edge/anime`).then(({ data }) => {
-      setData(data.data)
-    })
-  }, [])
+  async function fetchData (params) {
+    try {
+      const response = await axios.get(`http://localhost:3001/`)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // useEffect(() => {
+  //   axios.get(`https://kitsu.io/api/edge/categories`).then(({ data }) => {
+  //     setData(data.data)
+  //   })
+  // }, [])
 
   return (
     <div>
       <h1>Welcome to anime app</h1>
-      <button onClick={console.log(data)}>Fetch anime</button>
-      <div className='list' />
+      <button onClick={() => fetchData()}>Fetch anime</button>
+      <div className='list'>
+        {/* {data.length ? (
+          data.map(({ attributes: item }) => (
+            <div style={{ padding: '2rem' }} className='item_container'>
+              <div>{item.titles.en}</div>
+              <div>{item.synopsis}</div>
+            </div>
+          ))
+        ) : (
+          <div />
+        )} */}
+      </div>
     </div>
   )
 }
